ee3067f48bbece142a297daf40577b9d402070c7
xiaodao <826359422@qq.com>
2021-08-07 13:02:11 +0800

    upload

---
+++ src/cn/karent/leetcode_v6/LeetCode457.java
@@ -0,0 +1,45 @@
+package cn.karent.leetcode_v6;
+
+public class LeetCode457 {
+
+    private int next(int[] nums, int idx) {
+        int n = nums.length;
+        return ((idx + nums[idx]) % n + n) % n;
+    }
+
+    public boolean circularArrayLoop(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if( nums[i] == 0) continue;
+            int slow = i, fast = next(nums, i);
+            while ( nums[slow] * nums[fast] > 0
+                    && nums[slow] * nums[next(nums, fast)] > 0) {
+                if( slow == fast) {
+                    if( slow != next(nums, fast))
+                        return true;
+                    else break;
+                }
+                slow = next(nums, slow);
+                fast = next(nums, next(nums, fast));
+            }
+            for (int j = i; nums[j] * nums[next(nums, j)] > 0; ) {
+                int t = next(nums, j);
+                nums[j] = 0;
+                j = t;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        LeetCode457 lc = new LeetCode457();
+//        int[] arr = {2,-1,1,2,2};
+//        int[] arr = {-1, 2};
+//        int[] arr = {-2,1,-1,-2,-2};
+        int[] arr = {-2,-3,-9};
+        boolean ret = lc.circularArrayLoop(arr);
+        System.out.println(ret);
+    }
+
+
+}
